ec9d4a4ae6e45f2b8ba980ac746e567004810fe7
Edgard Romanovskiy <eromanovskiy01@qub.ac.uk>
2024-09-22 19:48:52 +0100

    Add vehicle steering and gas using input player input

--- Create with Code Part 1 Mission 1 - Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 1 - Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 20;
+    private float _speed = 20;
+    private float _turnSpeed = 45;
+    private float _forwardInput;
+    private float _horizontalInput;
 
     // Update is called once per frame
     void Update()
     {
+        _forwardInput = Input.GetAxis("Vertical");
+        _horizontalInput = Input.GetAxis("Horizontal");
         // Move vehicle forward
-        transform.Translate(transform.forward * Time.deltaTime * Speed);
+        transform.Translate(Vector3.forward * _speed * _forwardInput * Time.deltaTime);
+        // Steer car
+        transform.Rotate(Vector3.up, _turnSpeed * _horizontalInput * Time.deltaTime);
     }
 }

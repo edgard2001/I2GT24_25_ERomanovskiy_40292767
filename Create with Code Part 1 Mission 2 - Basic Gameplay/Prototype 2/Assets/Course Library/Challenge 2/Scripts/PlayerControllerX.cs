5cd905f84105daf630d1df6e3f766257d7546c7d
Edgard Romanovskiy <eromanovskiy01@qub.ac.uk>
2024-10-06 01:44:46 +0100

    Add Challenge 2 files with fixes

---
+++ Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype 2/Assets/Course Library/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float cooldown = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (cooldown <= 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = 1f;
+        }
+        if (cooldown > 0f)
+        {
+            cooldown -= Time.deltaTime;
+        }
+    }
+}

544b69609d4c2de9597c969e280e8206a26a1e48
Edgard Romanovskiy <eromanovskiy01@qub.ac.uk>
2024-10-06 01:15:46 +0100

    Add animals, food and player to the scene, add player movement

---
+++ Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 10.0f;
+    public float LimitX = 20.0f;
+    public GameObject Food;
+
+    private float _horizontalInput;
+
+    void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * _horizontalInput * Time.deltaTime * Speed);
+
+        if (Mathf.Abs(transform.position.x) > LimitX)
+            transform.position -= (transform.position.x - Mathf.Sign(transform.position.x) * LimitX) * transform.right;
+    }
+}

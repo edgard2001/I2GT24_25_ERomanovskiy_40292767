db59674b81fbd57d640185fcd10b21d8deb20ec7
edgard2001 <eromanovskiy303@gmail.com>
2024-10-29 01:21:20 +0000

    Attack and Block Particle Effects

---
+++ Project/Project/Assets/Weapon/FX/DestroyAfterEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterEffect : MonoBehaviour
+{
+
+    [SerializeField] private ParticleSystem _particleSystem;
+    
+    void Start()
+    {
+        float duration = _particleSystem.duration + _particleSystem.startLifetime;
+        Invoke(nameof(DestroySelf), duration);
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}

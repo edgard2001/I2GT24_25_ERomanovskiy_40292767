a3254c04287070d1b2e6b55213a277818f72b6c0
edgard2001 <eromanovskiy303@gmail.com>
2024-10-29 01:22:34 +0000

    Game Manager and Level Transitions

---
+++ Project/Project/Assets/Game/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    
+    void Awake()
+    {
+        if (_instance != null)
+            Destroy(gameObject);
+        else
+            _instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static GameManager GetInstance()
+    {
+        return _instance;
+    }
+
+    public void LoadMainScene()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        SceneManager.LoadScene(1);
+    }
+
+    public void RestartGame()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Project/Project/Assets/Game/HUD/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private GameManager _gameManager;
+    
+    void Start()
+    {
+        _gameManager = GameManager.GetInstance();
+    }
+
+    public void StartGame()
+    {
+        _gameManager.LoadMainScene();
+    }
+}

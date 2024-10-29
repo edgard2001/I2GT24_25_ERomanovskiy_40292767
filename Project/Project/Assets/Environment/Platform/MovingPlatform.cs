b2769075f07fe972aab17b81c733db1b2c8c399e
edgard2001 <eromanovskiy303@gmail.com>
2024-10-29 01:18:23 +0000

    Moving Platforms

---
+++ Project/Project/Assets/Environment/Platform/MovingPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Environment.Platform
+{
+    public class MovingPlatform : MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            _rigidbody.velocity = Mathf.Sin(Time.realtimeSinceStartup) * 5 * Vector3.up;
+            //_transform.rotation = Quaternion.AngleAxis(40 * Time.deltaTime, Vector3.up) * _transform.rotation;
+        }
+    }
+
+}

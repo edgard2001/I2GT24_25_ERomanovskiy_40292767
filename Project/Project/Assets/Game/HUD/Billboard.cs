15819d0df358fe6c478d1dbd0a2e2de546053182
edgard2001 <eromanovskiy303@gmail.com>
2024-10-29 01:14:13 +0000

    Enemy Health UI

---
+++ Project/Project/Assets/Game/HUD/Billboard.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Transform _transform;
+    private Transform _cameraTransform;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _transform = transform;
+        _cameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _transform.rotation = Quaternion.LookRotation(_cameraTransform.position - _transform.position);
+    }
+}

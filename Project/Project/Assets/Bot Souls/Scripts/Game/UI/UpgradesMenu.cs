51bccaa28744471915070952d30949f3838c932d
edgard2001 <eromanovskiy303@gmail.com>
2024-11-25 22:52:44 +0000

    Fix windows build issue

--- Project/Project/Assets/Bot Souls/Scripts/Game/UI/UpgradesMenu.cs
+++ Project/Project/Assets/Bot Souls/Scripts/Game/UI/UpgradesMenu.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Collectible;
 using TMPro;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class UpgradesMenu : MonoBehaviour
